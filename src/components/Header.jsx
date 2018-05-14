40e0e6bc4ebeef98fbd667231be5d955fe4c0f2c
Eric <eric.swotinsky@gmail.com>
2018-05-14 16:45:55 -0700

    refactor Header; lint components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,37 +1,90 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Collapse, DropdownToggle, DropdownMenu, DropdownItem, Nav, Navbar, NavbarToggler, NavbarBrand, NavItem, NavLink, UncontrolledDropdown } from 'reactstrap';
 
-function Header(){
-  return (
-    <div className="header">
-      <style jsx>{`
-          div.header {
-            right: 0;
-            top: 0;
-            left: 0;
-            padding: 1rem;
-            background-color: cyan;
-            text-align: center;
-            font-family:sans-serif;
-          }
-          img {
-            width:10%;
-            border-radius:40px;
-          }
-          span.header-link {
-            margin:5%;
-          }
-        `}
-      </style>
-      <Link to='/'><h1>Zazu's House</h1></Link>
-      <Link to='/about'><span className='header-link'>About</span></Link>
-      <Link to='/volunteers'><span className='header-link'>Volunteers</span></Link>
-      <Link to='/gallery'><span className='header-link'>Gallery</span></Link>
-      <Link to='/contact'><span className='header-link'>Contact</span></Link>
-      <Link to='/support'><span className='header-link'>Support</span></Link>
-      <Link to='/donate'><span className='header-link'>Donate Now!</span></Link>
-    </div>
-  );
+class Header extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState ({
+      isOpen: !this.state.isOpen
+    });
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <div className="header">
+        <style jsx>{`
+            div.header {
+              right: 0;
+              top: 0;
+              left: 0;
+              padding: 1rem;
+              background-color: lightgreen;
+              text-align: center;
+              font-family:sans-serif;
+            }
+            img {
+              width:10%;
+              border-radius:40px;
+            }
+          `}
+        </style>
+        <Navbar light expand="md">
+          <NavbarBrand href="/"><h2>Zazu's House</h2></NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className='ml-auto' navbar>
+              <NavItem>
+                <NavLink><Link to='/about'>About</Link></NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink><Link to='/volunteers'>Volunteers</Link></NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink><Link to='/gallery'>Gallery</Link></NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink><Link to='/contact'>Contact</Link></NavLink>
+              </NavItem>
+
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Support
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                    Option 1
+                  </DropdownItem>
+                  <DropdownItem>
+                    Option 2
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+
+              <NavItem>
+                <NavLink><Link to='/support'>Support</Link></NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink><Link to='/donate'>Donate Now</Link></NavLink>
+              </NavItem>
+
+
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
 }
 
 export default Header;
