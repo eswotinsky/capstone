04d4744ffac31d0637694427d546bc5bdc6a61a9
Eric <eric.swotinsky@gmail.com>
2018-05-15 13:01:52 -0700

    add social icons to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import faFacebook from '@fortawesome/fontawesome-free-brands/faFacebook';
+import faTwitter from '@fortawesome/fontawesome-free-brands/faTwitter';
+import faYoutube from '@fortawesome/fontawesome-free-brands/faYoutube';
+import faTumblr from '@fortawesome/fontawesome-free-brands/faTumblr';
+import faInstagram from '@fortawesome/fontawesome-free-brands/faInstagram';
 
 function Footer(){
   return (
@@ -16,6 +22,11 @@ function Footer(){
           `}
       </style>
       <h1>FOOTER COMPONENT</h1>
+      <a href='https://www.youtube.com/channel/UCf8LtsnnXnwAmXOFGhp_wjw/'><FontAwesomeIcon icon={faYoutube} size='lg' pull='right' /></a>
+      <a href='http://zazushousesanctuary.tumblr.com/'><FontAwesomeIcon icon={faTumblr} size='lg' pull='right' /></a>
+      <a href='https://www.instagram.com/zazushousesanctuary/'><FontAwesomeIcon icon={faInstagram} size='lg' pull='right' /></a>
+      <a href='https://twitter.com/zazushouse/'><FontAwesomeIcon icon={faTwitter} size='lg' pull='right' /></a>
+      <a href='http://www.facebook.com/zazushouse.parrotsanctuary'><FontAwesomeIcon icon={faFacebook} size='lg' pull='right' /></a>
     </div>
   );
 }
